5b4a4a7683cdaf23cd05fc05d3814259fc979a39
Kambaia Alberto <kambaiaalberto@gmail.com>
2022-05-06 22:43:27 +0100

    change a site key

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -77,7 +77,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6LcAu-IdAAAAAJOTI5E_eRltZNQCvukIl2-f1glQ"
+          sitekey="6LfFWs0fAAAAAAp3MBxHCdMZPiAAof-YyAMGafaM"
           onChange={(e) => {
             setIsHuman(true)
           }}
