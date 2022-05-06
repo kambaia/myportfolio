f6a261fabaa4789756c3b5a9e52442654486e82a
Kambaia Alberto <kambaiaalberto@gmail.com>
2022-05-06 23:05:40 +0100

    ...

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,6 @@
 import { Container, ContainerSucces } from './styles'
 import { useForm, ValidationError } from '@formspree/react'
 import { toast, ToastContainer } from 'react-toastify'
-import ReCAPTCHA from 'react-google-recaptcha'
 import { useEffect, useState } from 'react'
 import validator from 'validator'
 
@@ -9,7 +8,6 @@ export function Form() {
   const [state, handleSubmit] = useForm('myyozglw')
 
   const [validEmail, setValidEmail] = useState(false)
-  const [isHuman, setIsHuman] = useState(false)
   const [message, setMessage] = useState('')
 
   function verifyEmail(email: string) {
@@ -76,15 +74,10 @@ export function Form() {
           field="message"
           errors={state.errors}
         />
-        <ReCAPTCHA
-          sitekey="6LfFWs0fAAAAAAp3MBxHCdMZPiAAof-YyAMGafaM"
-          onChange={(e) => {
-            setIsHuman(true)
-          }}
-        ></ReCAPTCHA>
+       
         <button
           type="submit"
-          disabled={state.submitting || !validEmail || !message || !isHuman}
+          disabled={state.submitting || !validEmail || !message}
         >
           Enviar
         </button>
